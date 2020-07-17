5466a813dc3cf40a1b399962f7abf6f842255b7b
Philipp Schmid <philippschmid@Philipps-MacBook-Pro-2.local>
2020-07-17 12:05:42 +0200

    trying to add fonts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,35 +1,40 @@
 import { Component, OnInit } from "@angular/core";
-// import { FormFieldTypes } from "@aws-amplify/ui-components";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html"
 })
 export class LoginComponent implements OnInit {
-  // formFields: FormFieldTypes;
-
-  constructor() {
-    // this.formFields = [
-    //   {
-    //     type: "email",
-    //     label: "Custom email Label",
-    //     placeholder: "custom email placeholder",
-    //     required: true
-    //   },
-    //   {
-    //     type: "password",
-    //     label: "Custom Password Label",
-    //     placeholder: "custom password placeholder",
-    //     required: true
-    //   },
-    //   {
-    //     type: "phone_number",
-    //     label: "Custom Phone Label",
-    //     placeholder: "custom Phone placeholder",
-    //     required: false
-    //   }
-    // ];
-  }
+  signUpConfig = {
+    header: "Registriere dich als Student",
+    hideAllDefaults: true,
+    defaultCountryCode: "1",
+    signUpFields: [
+      {
+        label: "E-Mail Adresse",
+        key: "email",
+        required: true,
+        displayOrder: 1,
+        type: "string"
+      },
+      {
+        label: "Passwort",
+        key: "password",
+        required: true,
+        displayOrder: 2,
+        type: "password"
+      },
+      {
+        label: "Stadt",
+        key: "city",
+        required: false,
+        displayOrder: 4,
+        type: "string",
+        custom: true
+      }
+    ]
+  };
+  constructor() {}
 
   ngOnInit() {}
 }
